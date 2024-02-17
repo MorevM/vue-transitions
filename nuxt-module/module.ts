a6fa38f1f3cd08c2209a96e74bbc73eefd127fc7
MorevM <max.seainside@gmail.com>
2024-02-17 17:30:56 +0300

    feat(nuxt)!: Make sure the module runs with no compilation errors using Nuxt 2
    BREAKING CHANGE: Minimal supported Nuxt version now is 2.17.0
    
    As the module uses supporting utilities library that is not transpiled, additional transpilation step is required on the Nuxt side. Nuxt < 2.17.0 has some intrinsic limitations that are impractical to solve.

--- nuxt-module/module.ts
+++ nuxt-module/module.ts
@@ -1,28 +1,48 @@
-import { existsSync, unlinkSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
-import { mergeObjects, isEmpty } from '@morev/utils';
-import { defineNuxtModule, createResolver, addComponentsDir } from '@nuxt/kit';
+import { existsSync, unlinkSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs';
+import { mergeObjects, isEmpty, isArray } from '@morev/utils';
+import { defineNuxtModule, createResolver, addComponentsDir, isNuxt2 } from '@nuxt/kit';
 import type { PluginOptions } from '../types';
 
 const COMPONENTS = ['TransitionExpand', 'TransitionFade', 'TransitionScale', 'TransitionSlide'] as const;
+const BABEL_PLUGIN_NAME = '@babel/plugin-transform-logical-assignment-operators';
+const MODULE_NAME = '@morev/vue-transitions';
 
 export default defineNuxtModule<PluginOptions>({
 	meta: {
-		name: '@morev/vue-transitions/nuxt',
+		name: `${MODULE_NAME}/nuxt`,
 		configKey: 'vueTransitions',
+		compatibility: {
+			nuxt: '>= 2.17.0 || >=3.5.0',
+		},
 	},
-	defaults: {},
+	defaults: {
+		componentDefaultProps: {},
+		defaultProps: {},
+	},
 	async setup(options, nuxt) {
 		const DIRECTORY_NAME = 'vue-transitions';
 		const resolver = createResolver(import.meta.url);
-		const entries = options.components ?? ALL_COMPONENTS;
-
-		// If there are no components to register.
-		if (!Object.values(entries).filter(Boolean).length) return;
 
 		nuxt.options.css ??= [];
-		nuxt.options.css.push(`@morev/vue-transitions/styles`);
+		nuxt.options.css.push(`${MODULE_NAME}/styles`);
 
-		nuxt.options.build.transpile.push('@morev/utils');
+		// This is necessary because the package uses utilities
+		// that use modern syntax and have not been transpiled.
+		if (isNuxt2()) {
+			nuxt.options.build.transpile.push('@morev/utils', 'ohash', MODULE_NAME);
+
+			/* @ts-expect-error -- Lack of compatibility with Nuxt 2 */
+			nuxt.options.build.babel.plugins ??= [];
+			/* @ts-expect-error -- Lack of compatibility with Nuxt 2 */
+			const doesBabelPluginExists = nuxt.options.build.babel.plugins.some((plugin) => {
+				return isArray(plugin)
+					? plugin[0] === BABEL_PLUGIN_NAME
+					: plugin === BABEL_PLUGIN_NAME;
+			});
+
+			/* @ts-expect-error -- Lack of compatibility with Nuxt 2 */
+			!doesBabelPluginExists && nuxt.options.build.babel.plugins.push(BABEL_PLUGIN_NAME);
+		}
 
 		const templateContents = readFileSync(resolver.resolve('template.vue'), { encoding: 'utf8' });
 		const componentsDir = resolver.resolve(DIRECTORY_NAME);
