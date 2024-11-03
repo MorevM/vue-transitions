fc47d08cc76c0931254bbb861e20366d72d3565c
MorevM <max.seainside@gmail.com>
2024-11-03 18:27:44 +0300

    fix: Do not add `postinstall` entry in the `package.json` twice
    Closes #265

--- scripts/prepack.js
+++ scripts/prepack.js
@@ -2,10 +2,13 @@ import { readFileSync, writeFileSync } from 'node:fs';
 
 const packageJsonContents = readFileSync('package.json', { encoding: 'utf8' });
 
-writeFileSync(
-	'package.json',
-	packageJsonContents
-		.replace('"private": true', '"private": false')
-		.replace('"scripts": {', '"scripts": {\n\t\t"postinstall": "node ./scripts/postinstall.js",')
-		.replaceAll(/"workspaces": \[[^\]]*],\s*/g, ''),
-);
+let newContents = packageJsonContents
+	.replace('"private": true', '"private": false')
+	.replaceAll(/"workspaces": \[[^\]]*],\s*/g, '');
+
+if (!newContents.includes('postinstall')) {
+	newContents = packageJsonContents
+		.replace('"scripts": {', '"scripts": {\n\t\t"postinstall": "node ./scripts/postinstall.js",');
+}
+
+writeFileSync('package.json', newContents);
